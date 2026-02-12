84b772220ff58f8bc1f9ec3af7527f7b5be6708a
GubbinLubbin <silwalankit27@gmail.com>
2026-02-12 23:35:45 +0530

    Fix some socket set up error

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Server, Socket } from "socket.io";
-
+import { checkUserInConversation,createMessage } from "../services/chat.services.js";
 export const registerSocketHandlers = (io: Server) => {
 
   io.use((socket: Socket, next) => {
@@ -23,8 +23,22 @@ export const registerSocketHandlers = (io: Server) => {
   });
 
   io.on("connection", (socket: Socket) => {
+    const userId=socket.data.userId;
     console.log("User connected:", socket.data.userId);
+    socket.join(userId);
 
+
+    socket.on("send-message",async (data)=>{
+      const userId=socket.data.userId;
+      const {conversationId,content}=data;
+
+      const isMember=await checkUserInConversation(userId,conversationId);
+      if(!isMember){
+        return socket.emit("error","Unauthorized");
+      }
+      const message=await createMessage(conversationId, userId, content);
+      io.to(conversationId).emit("new-message",message);
+    })
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.data.userId);
     });
